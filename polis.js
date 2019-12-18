2c00b4271227c9754662ed24a0d7e3093487b627
Michael Meesseman <michael.meesseman@yahoo.com>
2019-12-18 09:42:51 -0500

    update

--- polis.js
+++ polis.js
@@ -7,9 +7,8 @@ var PORT = process.env.PORT || 9000;
 var app = express();
 
 function payloadProcessor (payload, done) {
-  var jsonString = payload.postData;
-  console.log(jsonString);
-  var pay = JSON.parse(jsonString);
+  var pay = JSON.parse(payload);
+  console.log(pay);
   if(pay.data.form_id == 'cdaa6515-0476-4b45-8f9c-a4a93d5c404c'){
     var eventId = pay.data.form_values['6fc3'];
     if(pay.type === 'record.create'){
